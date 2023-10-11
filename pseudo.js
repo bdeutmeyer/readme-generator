8007fae12991b44355cea4287a4389a85be03158
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-11 07:53:34 -0500

    Fleshed out questions and prompts, began other stuff

---
+++ pseudo.js
@@ -0,0 +1,4 @@
+//readme.md file created but not immediately visible
+//questions to prompt user for information
+//program would know which section to put user prompts into
+//file created once questions are answered
